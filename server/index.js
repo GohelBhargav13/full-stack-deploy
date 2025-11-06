649e27728a7d1f28b7074eb607137f08813e1625
= <gohelbhargav401@gmail.com>
2025-11-07 00:36:48 +0530

    production url is added

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ app.use(cors({
     origin:[
         "http://localhost:5173",
         "http://localhost:5174",
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "http://13.50.251.70:5174"
     ],
     methods:["GET","POST"],
     credentials:true
