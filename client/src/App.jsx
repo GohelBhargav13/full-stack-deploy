2d649b82e99ef92701a7b8ab2e77e70cc612ee89
= <gohelbhargav401@gmail.com>
2025-11-06 23:38:57 +0530

    First Step: 1

---
+++ client/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [data,setData] = useState([])
+
+  useEffect(() => {
+
+    fetch("http://localhost:5000/get-new")
+    .then((res) => {
+      console.log(res)
+      if(!res.ok){
+        throw new Error("This response is not okay")
+      }
+
+      return res.json()
+    })
+    .then((data) => {
+      console.log(data)
+      setData(data?.data)
+    })
+    .catch((err) => console.log("Error While fetching a data : ", err))
+
+  },[])
+
+  return (
+    <>
+      <h1 style={{ "textAlign":"center","marginTop":"60px" }}>Welcome to the profile-repo</h1>
+      <div id='main'>
+        { data.length > 0 && data.map((d,idx) => (
+          <div key={idx} id='main-div'>
+              <span>{ d?.name }</span>
+              <ul>
+                { d?.interest.map((skill,idx) => (
+                  <li key={idx}>{ skill }</li>
+                )) }
+              </ul>
+          </div>
+        )) }
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({
+    origin:[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:3000"
+    ],
+    methods:["GET","POST"],
+    credentials:true
+}))
+
+
+app.get("/",(req,res) => {
+    res.json({ message:"Hello from the root directory" })
+})
+
+app.get("/get-new", (req,res) => {
+
+    const data = [
+        { name:"bhargav", interest:["WEB","API","BACKEND","DEVOPS"] },
+        { name:"chhayank", interest:["PYTHON","AI","AUTOMATION","ML"] },
+        { name:"Aditya", interest:["NETWORKS","CYBER-SECURITY","INFORMATION-SECURITY"] },
+        { name:"Raj", interest:["MOBILE-DEVELOPMENT","FLUTTER","DART"] },
+        { name:"Arya", interest:["FLUTTER","DART","CROSS-PLATFORM DEVELOPMENT"] },
+        { name:"Nayan", interest:["PHP","JS","MYSQL","WEB-DEVELOPMENT"] }
+    ]
+
+    res.json({ data,message:"Data Fetched Successfully" })
+})
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`Server is running on the http://localhost:${PORT}`))
